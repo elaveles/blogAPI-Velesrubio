a711dc1c953d15de9c692142f9ccc657adfad09c
Ela <elavelesrubio@gmail.com>
2024-11-05 08:37:46 +0800

    Updated corsOptions

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: ['http://localhost:8000', 'http://localhost:3000'],
+  origin: ['http://localhost:8000', 'http://localhost:3000', 'https://blog-app-rho-mocha.vercel.app/'],
   credentials: true,
   optionsSuccessStatus: 200,
 }
